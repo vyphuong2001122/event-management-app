a9974a19bcc99c542764ce47b5dbdd29e1416943
vyphuong2001122 <519H0363@student.tdtu.edu.vn>
2024-12-07 23:10:11 +0700

    [FE] Add colors used in project

---
+++ frontend/lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color primaryColor = Color(0xFF4A90E2);
+const Color primaryColorLight = Color(0xFF87BFF3);
+const Color primaryColorLightest = Color(0xFFD8EAFE);
+const Color secondaryColor = Color(0xFFFF6F61);
+const Color secondaryColorLight = Color(0xFFFFA397);
+const Color secondaryColorLightest = Color(0xFFFFDED9);
+const Color backgroundColor = Colors.white;
