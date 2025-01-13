3d0b01b07af816d1f020290dcc81aa918ba82acf
LAPTOP-OTBR3VAC\ASUS <thinh44454@gmail.com>
2025-01-13 09:34:27 +0700

    [BE] new push

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,36 +1,30 @@
 const sequelize = require('../config/db.config');
 const User = require('./User');
 const Event = require('./Event');
-const Ticket = require('./Ticket');
-const Notification = require('./Notification');
-const Schedule = require('./Schedule');
-const Feedback = require('./Feedback');
-const Speaker = require('./Speaker');
-const Guest = require('./Guest');
+//const Notification = require('./Notification');
+//const Schedule = require('./Schedule');
+//const Feedback = require('./Feedback');
+////const Speaker = require('./Speaker');
+//const Guest = require('./Guest');
 const EventRegistration = require('./EventRegistration');
 
 
 // Define relationships
-User.hasMany(Ticket, { foreignKey: 'userId' });
-Ticket.belongsTo(User, { foreignKey: 'userId' });
 
-Event.hasMany(Ticket, { foreignKey: 'eventId' });
-Ticket.belongsTo(Event, { foreignKey: 'eventId' });
+//Event.hasMany(Schedule, { foreignKey: 'eventId' });
+//Schedule.belongsTo(Event, { foreignKey: 'eventId' });
 
-Event.hasMany(Schedule, { foreignKey: 'eventId' });
-Schedule.belongsTo(Event, { foreignKey: 'eventId' });
+//Event.hasMany(Feedback, { foreignKey: 'eventId' });
+//Feedback.belongsTo(Event, { foreignKey: 'eventId' });
 
-Event.hasMany(Feedback, { foreignKey: 'eventId' });
-Feedback.belongsTo(Event, { foreignKey: 'eventId' });
+//User.hasMany(Feedback, { foreignKey: 'userId' });
+//Feedback.belongsTo(User, { foreignKey: 'userId' });
 
-User.hasMany(Feedback, { foreignKey: 'userId' });
-Feedback.belongsTo(User, { foreignKey: 'userId' });
+//Event.hasMany(Speaker, { foreignKey: 'eventId' });
+//Speaker.belongsTo(Event, { foreignKey: 'eventId' });
 
-Event.hasMany(Speaker, { foreignKey: 'eventId' });
-Speaker.belongsTo(Event, { foreignKey: 'eventId' });
-
-Event.hasMany(Guest, { foreignKey: 'eventId' });
-Guest.belongsTo(Event, { foreignKey: 'eventId' });
+//Event.hasMany(Guest, { foreignKey: 'eventId' });
+//Guest.belongsTo(Event, { foreignKey: 'eventId' });
 
 User.hasMany(EventRegistration, { foreignKey: 'userId' });
 EventRegistration.belongsTo(User, { foreignKey: 'userId' });
@@ -44,11 +38,10 @@ module.exports = {
     sequelize,
     User,
     Event,
-    Ticket,
-    Notification,
-    Schedule,
-    Feedback,
-    Speaker,
-    Guest,
+    //Notification,
+    //Schedule,
+    //Feedback,
+    //Speaker,
+    //Guest,
     EventRegistration,
 };
